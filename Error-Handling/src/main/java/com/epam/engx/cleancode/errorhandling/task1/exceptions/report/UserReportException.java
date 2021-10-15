66289dff22a5d21adffdba6c9a80f1c983b71cec
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-10-15 10:34:51 +0300

    Fix according to comments

---
+++ Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/exceptions/report/UserReportException.java
@@ -0,0 +1,7 @@
+package com.epam.engx.cleancode.errorhandling.task1.exceptions.report;
+
+public class UserReportException extends RuntimeException {
+    public UserReportException(String message) {
+        super(message);
+    }
+}
